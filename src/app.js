6731f3445812845632fe11784f896597b2f5d95b
Luper95 <luisenriqueperez2605@hotmail.com>
2025-04-24 13:38:35 +0000

    optimizacion

--- src/app.js
+++ src/app.js
@@ -12,97 +12,79 @@ let playerWon = true;
 let pcWon = true;
 let button1 = document.getElementById('playerButton');
 let button2 = document.getElementById('pcButton');
+// texto de la eleccion del player
+let playerText = document.getElementById("choice");
+//texto de ganador
+let winnerText = document.getElementById("win");
+//iconos para mostrar, Piedra, tijera, papel, lagarto, spock
+let iconsArray = ['<i class="fa-solid fa-hand-back-fist"> </i>', '<i class="fa-solid fa-hand-scissors"></i>', '<i class="fa-solid fa-hand"> </i>', '<i class="fa-solid fa-hand-lizard"></i>', '<i class="fa-solid fa-hand-spock"></i>'];
+
 //boton de piedra
 const rockButton = document.getElementById("rock");
-rockButton.addEventListener("click", function () {
-  pcSelection();
-  selectionByPlayer = "piedra";
-  button1.innerHTML= '<i class="fa-solid fa-hand-back-fist"> </i>';
-  document.getElementById("choice").innerHTML = "Tu elegiste: piedra";
-  document.getElementById("win").innerHTML = selectWinner(selectionByPc, selectionByPlayer);
-  showWinner();
-  console.log(selectionByPlayer);
+
+//añadir listener a la piedra
+rockButton.addEventListener("click", () => {
+  playTheGame('piedra', iconsArray[0], 'Piedra');
 });
+
+
 //boton de tijera 
 const scizButton = document.getElementById("tijera");
 scizButton.addEventListener("click", function () {
-  pcSelection();
-  selectionByPlayer = "tijera";
-  button1.innerHTML='<i class="fa-solid fa-hand-scissors"></i>';
-  document.getElementById("choice").innerHTML = "Tu elegiste: Tijera";
-  document.getElementById("win").innerHTML = selectWinner(selectionByPc, selectionByPlayer);
-  showWinner();
-  console.log(selectionByPlayer);
+ playTheGame('tijeta',iconsArray[1],'Tijera');
 });
 //boton de papel
 const papperButton = document.getElementById("papel");
 papperButton.addEventListener("click", function () {
-  pcSelection();
-  selectionByPlayer = "papel";
-  button1.innerHTML='<i class="fa-solid fa-hand"> </i>';
-  document.getElementById("choice").innerHTML = "Tu elegiste: Papel";
-  document.getElementById("win").innerHTML = selectWinner(selectionByPc, selectionByPlayer);
-  showWinner();
-  console.log(selectionByPlayer);
+ playTheGame('papel',iconsArray[2],'Papel');
 });
 //boton lagarto
 const lizardButton = document.getElementById("lagarto");
 lizardButton.addEventListener("click", function () {
-  pcSelection();
-  selectionByPlayer = "lagarto";
-  button1.innerHTML='<i class="fa-solid fa-hand-lizard"></i>';
-  document.getElementById("choice").innerHTML = "Tu elegiste: Lagarto";
-  document.getElementById("win").innerHTML = selectWinner(selectionByPc, selectionByPlayer);
-  showWinner();
-  console.log(selectionByPlayer);
+ playTheGame('lagarto',iconsArray[3],'Lagarto');
 });
 // boton de spock
 const spockButton = document.getElementById("spock");
 spockButton.addEventListener("click", function () {
-  pcSelection();
-  selectionByPlayer = "spock";
-  button1.innerHTML='<i class="fa-solid fa-hand-spock"></i>';
-  document.getElementById("choice").innerHTML = "Tu elegiste: Spock";
-  document.getElementById("win").innerHTML = selectWinner(selectionByPc, selectionByPlayer);
-  showWinner();
-  console.log(selectionByPlayer);
+  playTheGame('spock',iconsArray[4],'Spock')
 });
 
+//Seleccion de la pc
 function pcSelection() {
   let randomNum = (Math.floor(Math.random() * 5) + 1);
   switch (randomNum) {
     case 1:
       document.getElementById("pcChoice").innerHTML = "La pc eligio: piedra";
       selectionByPc = "piedra";
-      button2.innerHTML= '<i class="fa-solid fa-hand-back-fist"> </i>';
+      button2.innerHTML = '<i class="fa-solid fa-hand-back-fist"> </i>';
       return;
     case 2:
       document.getElementById("pcChoice").innerHTML = "La pc eligio: Papel";
       selectionByPc = "papel";
-      button2.innerHTML='<i class="fa-solid fa-hand"> </i>';
+      button2.innerHTML = '<i class="fa-solid fa-hand"> </i>';
       return;
     case 3:
       document.getElementById("pcChoice").innerHTML = "La pc eligio: Tijera";
       selectionByPc = "tijera";
-      button2.innerHTML='<i class="fa-solid fa-hand-scissors"></i>';
+      button2.innerHTML = '<i class="fa-solid fa-hand-scissors"></i>';
       return;
     case 4:
       document.getElementById("pcChoice").innerHTML = "La pc eligio: Lagarto";
       selectionByPc = "lagarto";
-      button2.innerHTML='<i class="fa-solid fa-hand-lizard"></i>';
+      button2.innerHTML = '<i class="fa-solid fa-hand-lizard"></i>';
       return;
     case 5:
       document.getElementById("pcChoice").innerHTML = "La pc eligio: Spock";
       selectionByPc = "spock";
-      button2.innerHTML='<i class="fa-solid fa-hand-spock"></i>';
+      button2.innerHTML = '<i class="fa-solid fa-hand-spock"></i>';
       return;
     default:
       break;
   }
 }
-
+//elegir ganador
 function selectWinner(pcChoice, playerChoice) {
-  console.log('estoy siendo llamado');
+ 
   switch (playerChoice) {
     case 'piedra':
       if (pcChoice === playerChoice) {
@@ -119,7 +101,7 @@ function selectWinner(pcChoice, playerChoice) {
           spread: 70,
           origin: { y: 0.6 }
         });
-        
+
         return 'Gano el Jugador';
       }
       else {
@@ -220,7 +202,7 @@ function selectWinner(pcChoice, playerChoice) {
 
   }
 }
-
+//mostrar ganador
 function showWinner() {
 
   if (pcWon === playerWon) {
@@ -242,5 +224,14 @@ function showWinner() {
     button2.classList.add('btn-success');
 
   }
+}
+//funcion para que no se repita elcodigo tanto.
+function playTheGame(playerSelection, iconToShow, selectionToShow) {  
+  pcSelection();
+  selectionByPlayer = playerSelection;
+  button1.innerHTML = iconToShow;
+  playerText.innerHTML = "Tu elegiste: " + selectionToShow;
+  winnerText.innerHTML = selectWinner(selectionByPc, selectionByPlayer);
+  showWinner();
 }
 
